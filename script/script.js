1aba708a4f1c775de60f46665bb376d3fbda2ba9
Abhijith Gaganan <theab10hd@gmail.com>
2024-02-02 08:56:23 +0530

    Portfolio
    includes -
    HTML5
    CSS
    JavaScript

---
+++ script/script.js
@@ -0,0 +1,24 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// const observer = new IntersectionObserver((entries) =>{
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show');
+//         }
+//         else{
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
+
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
